330ace996964372a687728e8fcf6728a18cb8e86
Syed Ali Shah <44297283+ali-shahKust@users.noreply.github.com>
2025-02-25 12:27:18 +0500

    Update DeviceInformationPlugin.java
    Fixing flutter issues

--- android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
+++ android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
@@ -24,7 +24,12 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
 
 /** DeviceInformationPlugin */
 public class DeviceInformationPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
